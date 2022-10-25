33c2ec1bff2944168c0208aa61497c7e29d0e679
Greg Bolsinga <bolsinga@mac.com>
2022-10-25 14:18:33 -0700

    Implement action handler for partial images in-app
    - This just copies the string to the clipboard as the script gets debugged.
    - In the future, this can run from the app, but requires additional app setup

--- MissingArt/MissingArtApp.swift
+++ MissingArt/MissingArtApp.swift
@@ -12,7 +12,32 @@ import SwiftUI
 struct MissingArtApp: App {
   var body: some Scene {
     WindowGroup {
-      MissingArtworkView()
+      MissingArtworkView { missingArtwork in
+        Button("Copy Partial Art AppleScript") {
+          let escapedRepresentation = "\(missingArtwork.simpleRepresentation)".replacingOccurrences(of: "\"", with: "\\\"")
+          let appleScript = """
+            tell application "Music"
+              set results to search the first library playlist for "\(escapedRepresentation)"
+              set imageData to missing value
+              repeat with trk in results
+                if (count of artworks of trk) is not 0 then
+                  set imageData to raw data of item 1 of artworks of trk
+                  exit repeat
+                end if
+              end repeat
+              repeat with trk in results
+                set artwrk to item 1 of artworks of trk
+                if artwrk is missing value then
+                  set raw data of artwrk to imageData
+                end if
+              end repeat
+            end tell
+            """
+          let pasteboard = NSPasteboard.general
+          pasteboard.clearContents()
+          pasteboard.setString(appleScript, forType: .string)
+        }
+      }
     }
   }
 }
