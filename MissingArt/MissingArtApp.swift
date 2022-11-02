57fbb16fe14a4fbe46ba511803d33c6655c7f5d9
Greg Bolsinga <bolsinga@mac.com>
2022-11-02 09:58:12 -0700

    Filter the search results (#22)
    Need a broader search for this to work, and then have to filter it down with specifics.

--- MissingArt/MissingArtApp.swift
+++ MissingArt/MissingArtApp.swift
@@ -14,21 +14,45 @@ struct MissingArtApp: App {
     WindowGroup {
       MissingArtworkView { missingArtwork in
         Button("Copy Partial Art AppleScript") {
-          let escapedRepresentation = "\(missingArtwork.simpleRepresentation)".replacingOccurrences(of: "\"", with: "\\\"")
+          let searchStringRepresentation = "\(missingArtwork.simpleRepresentation)".replacingOccurrences(of: "\"", with: "\\\"")
+          var trackTest : String
+          switch missingArtwork {
+          case .ArtistAlbum(let artist, let album):
+            trackTest = "album of trk is equal to \"\(album)\" and artist of trk is equal to \"\(artist)\" then"
+          case .CompilationAlbum(let album):
+            trackTest = "album of trk is equal to \"\(album)\" then"
+          }
           let appleScript = """
             tell application "Music"
-              set results to search the first library playlist for "\(escapedRepresentation)"
+              set unfilteredResults to search the first library playlist for "\(searchStringRepresentation)"
+              set results to {}
+              repeat with trk in unfilteredResults
+                if \(trackTest)
+                  set the end of results to trk
+                end if
+              end repeat
               set imageData to missing value
               repeat with trk in results
                 if (count of artworks of trk) is not 0 then
                   set imageData to raw data of item 1 of artworks of trk
+                  log "found artwork"
                   exit repeat
                 end if
               end repeat
+              if imageData is missing value then
+                log "cannot find artwork"
+              end if
               repeat with trk in results
-                set artwrk to item 1 of artworks of trk
+                set artwrk to missing value
+                if artworks of trk is not missing value then
+                  set artwrk to item 1 of artworks of trk
+                end if
                 if artwrk is missing value then
+                  log "still no artwork for " & name of trk
+                end if
+                if artwrk is not missing value then
                   set raw data of artwrk to imageData
+                  log "set an artwork"
                 end if
               end repeat
             end tell
