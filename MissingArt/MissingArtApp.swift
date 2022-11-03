444dc6eee0fc10ec220ec849685fa83209876019
Greg Bolsinga <bolsinga@mac.com>
2022-11-02 19:21:47 -0700

    Make an AppleScript function to be called to update the images (#25)
    It takes a search term and a "function pointer" to call to ensure the search matches what is intended.
    The intent is to make this more reusable for multiple MissingArtworks.

--- MissingArt/MissingArtApp.swift
+++ MissingArt/MissingArtApp.swift
@@ -9,69 +9,96 @@ import MissingArtwork
 import SwiftUI
 
 extension MissingArtwork {
-  var appleScriptSearchRepresentation: String {
-    "\(simpleRepresentation)"
-      .replacingOccurrences(of: "\"", with: "\\\"")
+  private var appleScriptSearchRepresentation: String {
+    "\(simpleRepresentation)".replacingOccurrences(of: "\"", with: "\\\"")
   }
 
-  var appleScriptTrackTest: String {
+  private var appleScriptVerifyTrackMatch: String {
     switch self {
     case .ArtistAlbum(let artist, let album):
       return
-        "album of trk is equal to \"\(album)\" and artist of trk is equal to \"\(artist)\" then"
+        "album of trk is equal to \"\(album)\" and artist of trk is equal to \"\(artist)\""
     case .CompilationAlbum(let album):
-      return "album of trk is equal to \"\(album)\" then"
+      return "album of trk is equal to \"\(album)\""
     }
   }
 
-  var appleScriptPartialArtwork: String {
+  private var appleScriptVerifyTrackFunctionName: String {
+    "verify_track_\(String(simpleRepresentation.compactMap{ $0.isLetter ? $0 : "_" }))"
+  }
+
+  var appleScriptCodeToFixPartialArtwork: String {
+    let appleScriptVerifyTrackFunctionName = appleScriptVerifyTrackFunctionName
     return """
-      tell application "Music"
-        set unfilteredResults to search the first library playlist for "\(self.appleScriptSearchRepresentation)"
-        set results to {}
-        repeat with trk in unfilteredResults
-          if \(self.appleScriptTrackTest)
-            set the end of results to trk
-          end if
-        end repeat
-        set imageData to missing value
-        repeat with trk in results
-          if (count of artworks of trk) is not 0 then
-            set imageData to raw data of item 1 of artworks of trk
-            log "found artwork"
-            exit repeat
-          end if
-        end repeat
-        if imageData is missing value then
-          log "cannot find artwork"
-        end if
-        repeat with trk in results
-          set artwrk to missing value
-          if artworks of trk is not missing value then
-            set artwrk to item 1 of artworks of trk
-          end if
-          if artwrk is missing value then
-            log "still no artwork for " & name of trk
-          end if
-          if artwrk is not missing value then
-            set raw data of artwrk to imageData
-            log "set an artwork"
-          end if
-        end repeat
-      end tell
+          on \(appleScriptVerifyTrackFunctionName)(trk)
+          set matches to false
+          tell application "Music"
+            set matches to \(appleScriptVerifyTrackMatch)
+          end tell
+          return matches
+          end \(appleScriptVerifyTrackFunctionName)
+          fixPartialAlbum(\"\(appleScriptSearchRepresentation)\", \(appleScriptVerifyTrackFunctionName))
       """
   }
 }
 
 @main
 struct MissingArtApp: App {
+  let appleScriptFixPartialAlbumFunctionDefinition = """
+    on fixPartialAlbum(searchString, uncallableTrackTest)
+      global trackTest
+      set trackTest to uncallableTrackTest
+      tell application "Music"
+        set unfilteredResults to search the first library playlist for searchString
+      end tell
+      set results to {}
+      repeat with trk in unfilteredResults
+        if trackTest(trk) then
+          set the end of results to trk
+        end if
+      end repeat
+      set imageData to missing value
+      repeat with trk in results
+        tell application "Music"
+          if (count of artworks of trk) is not 0 then
+            set imageData to raw data of item 1 of artworks of trk
+            log "found artwork"
+            exit repeat
+          end if
+        end tell
+      end repeat
+      if imageData is missing value then
+        log "cannot find artwork"
+      end if
+      repeat with trk in results
+        set artwrk to missing value
+        tell application "Music"
+          if artworks of trk is not missing value then
+            set artwrk to item 1 of artworks of trk
+          end if
+        end tell
+        if artwrk is missing value then
+          log "no artwork for " & name of trk
+        end if
+        if artwrk is not missing value then
+          tell application "Music"
+            set raw data of artwrk to imageData
+          end tell
+        end if
+      end repeat
+    end fixPartialAlbum
+    """
   var body: some Scene {
     WindowGroup {
       MissingArtworkView { missingArtwork in
         Button("Copy Partial Art AppleScript") {
+          let appleScript = """
+            \(appleScriptFixPartialAlbumFunctionDefinition)
+            \(missingArtwork.appleScriptCodeToFixPartialArtwork)
+            """
           let pasteboard = NSPasteboard.general
           pasteboard.clearContents()
-          pasteboard.setString(missingArtwork.appleScriptPartialArtwork, forType: .string)
+          pasteboard.setString(appleScript, forType: .string)
         }
       }
     }
