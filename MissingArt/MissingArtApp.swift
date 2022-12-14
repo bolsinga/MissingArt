bolsinga/MissingArt

commit a01ebfa90047f4521906652957f806e530df0454

Add func to generate partial artwork AppleScript for an array of MissingArtwork (#47)
- This will allow batches of work once multiple selection is set up in the UI.
- convert existing single MissingArtwork clients to just pass an array of one.
- as a bonus wrap the handler call with try / on error.

MissingArt/MissingArtApp.swift
@@ -43,6 +43,12 @@ extension MissingArtwork {
           end tell
           return matches
           end \(appleScriptVerifyTrackFunctionName)
+
+      """
+  }
+
+  var appleScriptCodeToFixPartialArtworkCall: String {
+    return """
           fixAlbumArtwork(\"\(appleScriptSearchRepresentation)\", \(appleScriptVerifyTrackFunctionName), findPartialImage)
       """
   }
@@ -156,19 +162,39 @@ struct MissingArtApp: App {
         end if
       end repeat
     end fixAlbumArtwork
+
     """
 
-  private func partialArtworkAppleScript(_ missingArtwork: MissingArtwork) -> String {
-    return """
+  private func partialArtworksAppleScript(_ missingArtworks: [MissingArtwork]) -> String {
+    var appleScript = """
       \(appleScriptFixPartialAlbumFunctionDefinition)
-      \(missingArtwork.appleScriptCodeToFixPartialArtwork)
-      return true
+
       """
+    var calls = ""
+    for missingArtwork in missingArtworks {
+      appleScript.append(missingArtwork.appleScriptCodeToFixPartialArtworkDefinition)
+
+      calls.append(
+        """
+        try
+          \(missingArtwork.appleScriptCodeToFixPartialArtworkCall)
+        on error errorString
+          log \"Error Trying to Fix Artwork: \" & errorString
+        end try
+
+        """)
+    }
+    appleScript.append(calls)
+    appleScript.append(
+      """
+      return true
+      """)
+    return appleScript
   }
 
   private func copyPartialArtButton(_ missingArtwork: MissingArtwork) -> some View {
     Button("Copy Partial Art AppleScript") {
-      let appleScript = partialArtworkAppleScript(missingArtwork)
+      let appleScript = partialArtworksAppleScript([missingArtwork])
       let pasteboard = NSPasteboard.general
       pasteboard.clearContents()
       pasteboard.setString(appleScript, forType: .string)
@@ -181,7 +207,7 @@ struct MissingArtApp: App {
   }
 
   private func fixPartialArtwork(_ missingArtwork: MissingArtwork) throws {
-    let exec = NSAppleScript(source: partialArtworkAppleScript(missingArtwork))
+    let exec = NSAppleScript(source: partialArtworksAppleScript([missingArtwork]))
     if let exec = exec {
       var errorDictionary: NSDictionary?
       _ = exec.executeAndReturnError(&errorDictionary)

commit 9572558a34192cfa1ef68a1a751efd17d3034f67

Allow errors to be thrown when running AppleScript in process
- Broken by a01ebfa90047f4521906652957f806e530df0454
- When copy / pasting the AppleScript text, catch and logs errors for convenience
- When running the AppleScript in-process via NSAppleScript, do not catch the error and log. Let the native code find the error.

MissingArt/MissingArtApp.swift
@@ -15,7 +15,8 @@ struct MissingArtApp: App {
 
   private func copyPartialArtButton(_ missingArtwork: MissingArtwork) -> some View {
     Button("Copy Partial Art AppleScript") {
-      let appleScript = MissingArtwork.partialArtworksAppleScript([missingArtwork])
+      let appleScript = MissingArtwork.partialArtworksAppleScript(
+        [missingArtwork], catchAndLogErrors: true)
       let pasteboard = NSPasteboard.general
       pasteboard.clearContents()
       pasteboard.setString(appleScript, forType: .string)
@@ -24,7 +25,8 @@ struct MissingArtApp: App {
 
   private func copyArtButton(_ missingArtwork: MissingArtwork, image: NSImage) -> some View {
     Button("Copy Art AppleScript") {
-      let appleScript = MissingArtwork.artworksAppleScript([missingArtwork])
+      let appleScript = MissingArtwork.artworksAppleScript(
+        [missingArtwork], catchAndLogErrors: true)
       let pasteboard = NSPasteboard.general
       pasteboard.clearContents()
       // Put the image on the clipboard for the script. Needs to be first.
@@ -38,7 +40,8 @@ struct MissingArtApp: App {
   }
 
   private func fixPartialArtwork(_ missingArtwork: MissingArtwork) throws {
-    let exec = NSAppleScript(source: MissingArtwork.partialArtworksAppleScript([missingArtwork]))
+    let exec = NSAppleScript(
+      source: MissingArtwork.partialArtworksAppleScript([missingArtwork], catchAndLogErrors: false))
     if let exec = exec {
       var errorDictionary: NSDictionary?
       _ = exec.executeAndReturnError(&errorDictionary)
@@ -98,7 +101,8 @@ struct MissingArtApp: App {
         default:
           let partials = missingImages.filter { $0.availability == .some }.map { $0.missingArtwork }
           Button("Copy Multiple Partial Art AppleScript") {
-            let appleScript = MissingArtwork.partialArtworksAppleScript(partials)
+            let appleScript = MissingArtwork.partialArtworksAppleScript(
+              partials, catchAndLogErrors: true)
 
             let pasteboard = NSPasteboard.general
             pasteboard.clearContents()

MissingArt/MissingArtwork+AppleScript.swift
@@ -165,22 +165,32 @@ extension MissingArtwork {
     """
 
   private static func _artworksAppleScript(
-    _ missingArtworks: [MissingArtwork], caller: ((MissingArtwork) -> String)
+    _ missingArtworks: [MissingArtwork],
+    catchAndLogErrors: Bool,
+    caller: ((MissingArtwork) -> String)
   ) -> String {
     var appleScript = """
       \(appleScriptFixAlbumArtFunctionDefinition)
 
       """
     for missingArtwork in missingArtworks {
-      appleScript.append(
-        """
-        try
-          \(caller(missingArtwork))
-        on error errorString
-          log \"Error Trying to Fix Artwork: \" & errorString
-        end try
+      if catchAndLogErrors {
+        appleScript.append(
+          """
+          try
+            \(caller(missingArtwork))
+          on error errorString
+            log \"Error Trying to Fix Artwork: \" & errorString
+          end try
 
-        """)
+          """)
+      } else {
+        appleScript.append(
+          """
+            \(caller(missingArtwork))
+
+          """)
+      }
     }
     appleScript.append(
       """
@@ -189,14 +199,20 @@ extension MissingArtwork {
     return appleScript
   }
 
-  public static func partialArtworksAppleScript(_ missingArtworks: [MissingArtwork]) -> String {
-    return _artworksAppleScript(missingArtworks) { missingArtwork in
+  public static func partialArtworksAppleScript(
+    _ missingArtworks: [MissingArtwork], catchAndLogErrors: Bool
+  ) -> String {
+    return _artworksAppleScript(missingArtworks, catchAndLogErrors: catchAndLogErrors) {
+      missingArtwork in
       missingArtwork.appleScriptCodeToFixPartialArtworkCall
     }
   }
 
-  public static func artworksAppleScript(_ missingArtworks: [MissingArtwork]) -> String {
-    return _artworksAppleScript(missingArtworks) { missingArtwork in
+  public static func artworksAppleScript(
+    _ missingArtworks: [MissingArtwork], catchAndLogErrors: Bool
+  ) -> String {
+    return _artworksAppleScript(missingArtworks, catchAndLogErrors: catchAndLogErrors) {
+      missingArtwork in
       missingArtwork.appleScriptCodeToFixArtworkCall
     }
   }