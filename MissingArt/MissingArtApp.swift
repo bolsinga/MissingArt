bolsinga/MissingArt

commit 72dbc4f735f6c03be770d35fc30e0ef08e71f864

Refer to AppleScript in App now (#73)
- This way the App knows about the other 2 parts, but those parts do not know about each other.

MissingArt/MissingArtApp.swift
@@ -84,7 +84,10 @@ struct MissingArtApp: App {
               Button("Fix Partial Art") {
                 Task {
                   do {
-                    try await MissingArtwork.fixPartialArtwork(missingImage.missingArtwork)
+                    let script = try AppleScript(
+                      source: MissingArtwork.partialArtworksAppleScript(
+                        [missingImage.missingArtwork], catchAndLogErrors: false))
+                    try await script.run()
                   } catch let error as LocalizedError {
                     reportError(
                       FixArtError.cannotFixPartialArtwork(missingImage.missingArtwork, error))

MissingArt/MissingArtwork+AppleScript.swift
@@ -173,10 +173,4 @@ extension MissingArtwork {
       missingArtwork.appleScriptCodeToFixArtworkCall
     }
   }
-
-  public static func fixPartialArtwork(_ missingArtwork: MissingArtwork) async throws {
-    let script = try AppleScript(
-      source: MissingArtwork.partialArtworksAppleScript([missingArtwork], catchAndLogErrors: false))
-    try await script.run()
-  }
 }

commit 8c6d7f219c326a391ad7dc26b827934a2484da12

Run AppleScript on non-main again
- Broken by 72dbc4f735f6c03be770d35fc30e0ef08e71f864
- Calling a func annotated with async will "throw" this off onto another thread than main.

MissingArt/MissingArtApp.swift
@@ -30,6 +30,13 @@ extension FixArtError: LocalizedError {
   }
 }
 
+private extension MissingArtwork {
+  func fixPartialArtwork() async throws {
+    let script = try AppleScript(source: MissingArtwork.partialArtworksAppleScript([self], catchAndLogErrors: false))
+    try await script.run()
+  }
+}
+
 @main
 struct MissingArtApp: App {
 
@@ -84,10 +91,7 @@ struct MissingArtApp: App {
               Button("Fix Partial Art") {
                 Task {
                   do {
-                    let script = try AppleScript(
-                      source: MissingArtwork.partialArtworksAppleScript(
-                        [missingImage.missingArtwork], catchAndLogErrors: false))
-                    try await script.run()
+                    try await missingImage.missingArtwork.fixPartialArtwork()
                   } catch let error as LocalizedError {
                     reportError(
                       FixArtError.cannotFixPartialArtwork(missingImage.missingArtwork, error))