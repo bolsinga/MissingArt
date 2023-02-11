e3a288ef6690af3822386669a9f63b04e2b690fd
Greg Bolsinga <bolsinga@mac.com>
2023-02-10 20:48:02 -0800

    Separate FixArtError into its own file (#97)

--- MissingArt/MissingArtApp.swift
+++ MissingArt/MissingArtApp.swift
@@ -8,27 +8,6 @@
 import MissingArtwork
 import SwiftUI
 
-private enum FixArtError: Error {
-  case cannotFixArtwork(MissingArtwork, Error)
-}
-
-extension FixArtError: LocalizedError {
-  var errorDescription: String? {
-    switch self {
-    case .cannotFixArtwork(let missingArtwork, let error):
-      return
-        "Unable to change Music artwork image for \(missingArtwork.description): \(error.localizedDescription)"
-    }
-  }
-
-  var recoverySuggestion: String? {
-    switch self {
-    case .cannotFixArtwork(_, _):
-      return "The artwork was not able to be fixed. Try running as an AppleScript."
-    }
-  }
-}
-
 @main
 struct MissingArtApp: App {
 
